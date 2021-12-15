59e09d0057f59f46d0acf57e7fa0a8897a294d1c
jiyinyiyong <jiyinyiyong@gmail.com>
2021-12-16 01:05:03 +0800

    reuse tests code; update intros in README

--- tests/lexicon_key_test.rs
+++ tests/lexicon_key_test.rs
@@ -18,3 +18,68 @@ fn order_of_keys() {
   assert!(LexiconKey::new("aT") < LexiconKey::new("b"));
   assert!(LexiconKey::new("azzzzzz") < LexiconKey::new("b"));
 }
+
+#[test]
+fn test_insertion_beginning() -> Result<(), String> {
+  let mut k = LexiconKey::default();
+  for _ in 0..1000 {
+    let next = k.bisect_beginning()?;
+    assert!(next < k);
+    k = next;
+  }
+
+  Ok(())
+}
+
+#[test]
+fn test_insertion_end() -> Result<(), String> {
+  let mut k = LexiconKey::default();
+  for _ in 0..1000 {
+    let next = k.bisect_end()?;
+    assert!(k < next);
+    k = next;
+  }
+
+  Ok(())
+}
+
+#[test]
+fn test_insertion_middle() -> Result<(), String> {
+  let mut left = LexiconKey::new("a")?;
+  let right = LexiconKey::new("b")?;
+  for _ in 0..1000 {
+    let next = left.bisect(&right)?;
+    assert!(left < next);
+    assert!(next < right);
+    left = next;
+  }
+
+  let left = LexiconKey::new("a")?;
+  let mut right = LexiconKey::new("b")?;
+  for _ in 0..1000 {
+    let next = left.bisect(&right)?;
+    assert!(left < next);
+    assert!(next < right);
+    right = next;
+  }
+
+  let mut left = LexiconKey::new("a")?;
+  let mut right = LexiconKey::new("b")?;
+  let mut prefer_left = true;
+  for _ in 0..1000 {
+    let next = left.bisect(&right)?;
+    // println!("{} {} {}", left, next, right);
+    assert!(left < next);
+    assert!(next < right);
+
+    if prefer_left {
+      left = next;
+      prefer_left = false;
+    } else {
+      right = next;
+      prefer_left = true
+    }
+  }
+
+  Ok(())
+}
